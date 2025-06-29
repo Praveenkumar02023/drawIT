c618ce9983f20b2ade7cc93d8775bc7a9b945169
Praveen Kumar <Praveenkumar042023@gmail.com>
2025-06-29 15:48:21 +0000

    add create room zod schema.

--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,13 +1,19 @@
+
 import {z} from 'zod'
 export const signupValidator = z.object({
 
-    firstName : z.string(),
-    lastName : z.string(),
+    name : z.string(),
     email : z.string().email(),
     password : z.string()
+    
 });
 
 export const signinValidator = z.object({
     email : z.string().email(),
     password : z.string()
 });
+
+export const createRoomValidator = z.object({
+    adminId : z.string(),
+    slug : z.string()
+})
